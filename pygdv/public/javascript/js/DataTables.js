952505603752fb9f31b56947230fb92f957a958e
Yohan Jarosz <yohanjarosz@yahoo.fr>
2012-02-07 16:36:43 +0100

    add : preferences (on datatable.js) are now saved automatically with a cookie

--- pygdv/public/javascript/js/DataTables.js
+++ pygdv/public/javascript/js/DataTables.js
@@ -8,36 +8,9 @@ jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
 
 
 $(document).ready(function() {
-    // $('.grid tr').click( function() {
-    //     if ( $(this).hasClass('row_selected') ){
-    //         $(this).removeClass('row_selected');
-    // 	} else {
-    //         $(this).addClass('row_selected');
-    // 	}
-    // });
     
     $('.grid').dataTable( {
         "aaSorting": [ [0,'asc'], [1,'asc'] ],
 	"bStateSave" : true,
-
-	"fnCookieCallback" : function(sName, oData, sExpires, sPath){
-	    console.log('cook');
-	    console.log(sName);
-	    console.log(oData);
-	    console.log(sExpires);
-	    console.log(sPath);
-	},
-	
-	"fnDrawCallback" : function(sName, oData){
-	    console.log('draw');
-	    console.log(sName);
-	},
-	"fnServerParams" : function(sName, oData){
-	    console.log('serv');
-	    console.log(sName);
-	    console.log(oData);
-}
-
-
     });
 });
